c99515b287cd7a98b4fdff655b1835223b8b57df
Alexis CERDA <alexis.cerda-de-almeida-vilaca2@etu.univ-lorraine.fr>
2026-03-18 13:06:43 +0100

    feat: Change updateConfiguration endpoint to use PATCH method for partial updates

--- src/main/java/fr/prefecture/sidsic/dashboard_sidsic/controller/ConfigurationController.java
+++ src/main/java/fr/prefecture/sidsic/dashboard_sidsic/controller/ConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -48,7 +49,7 @@ public class ConfigurationController {
         }
     }
 
-    @PutMapping("/configurations")
+    @PatchMapping("/configurations")
     public ResponseEntity<?> updateConfiguration( @RequestBody ConfigurationDTO configurationDTO) {
         try {
             return ResponseEntity.ok(configurationService.updateConfiguration(configurationDTO.getId(), configurationDTO));
